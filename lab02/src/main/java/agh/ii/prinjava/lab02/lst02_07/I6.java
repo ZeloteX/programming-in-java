c42f1edd373d5eea4869cbd8db61db859cbd393b
ZeloteX <thomascw974@gmail.com>
2021-09-30 22:28:00 +0200

    Lab02 commit

---
+++ lab02/src/main/java/agh/ii/prinjava/lab02/lst02_07/I6.java
@@ -0,0 +1,7 @@
+package agh.ii.prinjava.lab02.lst02_07;
+
+interface I6 extends I5, I4 {
+} // an "aggregating" interface
+
+//interface I7 implements I5, I4 {} //  No implements clause allowed for interface
+
---
+++ lab02/src/main/java/agh/ii/prinjava/lab02/lst02_09/I41_TYPE.java
@@ -0,0 +1,9 @@
+package agh.ii.prinjava.lab02.lst02_09;
+
+// II. Factory pattern (an interface static method based, solution)
+
+
+enum I41_TYPE {
+    C41_IMPL_1,
+    C41_IMPL_2
+}
